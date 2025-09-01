0fc83ccc9409edebfe187c8964c0879ba992901d
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-09-01 22:49:48 +0900

    Fix clean build
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ cp -f ../prod-devel-rcar4_new.yaml ./
 if [[ "${CLEAN_BUILD_TEST}" == "yes" ]]; then
     sed -i -e 's/"yocto"/"yocto-clean"/' ./prod-devel-rcar4_new.yaml
     rm -rf yocto-clean/build-dom*/conf
-    #rm -rf ./yocto-clean/build-dom*
+    rm -rf ./yocto-clean/build-dom*
 fi
 
 rm -rf yocto/build-dom*/conf
