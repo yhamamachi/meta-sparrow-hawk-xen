5bf2ce0d01026b0bfc59e87fea6d252ba0b5ba36
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-10-28 22:22:11 +0900

    domd: install-files-doma: Restore idc files
    It seems that idc files for QEMU usb tablet is needed when using
    qemu-tablet-pci device.
    
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- meta-xen-domd-virtio/recipes-doma-support/install-files-doma/files/install-files-doma.sh
+++ meta-xen-domd-virtio/recipes-doma-support/install-files-doma/files/install-files-doma.sh
@@ -18,3 +18,25 @@ adb shell sync
 
 echo "apk files are installed"
 
+cat << EOS > tablet.idc
+touch.deviceType = touchScreen
+#touch.deviceType = pointer
+touch.gestureMode = spots
+#touch.gestureMode = pointer
+touch.orientationAware = 1
+touch.toolSize.calibration = default
+touch.pressure.calibration = default
+touch.size.calibration = default
+touch.orientation.calibration = none
+device.internal = 1
+EOS
+
+adb shell mkdir -p /data/system/devices/idc
+adb push tablet.idc /data/system/devices/idc/Vendor_0627_Product_0003.idc
+adb shell chown system:system -R /data/system/devices
+adb shell chmod 644 -R /data/system/devices/idc/*.idc
+adb shell sync
+adb reboot
+
+echo "idc files are installed. Rebooting."
+
