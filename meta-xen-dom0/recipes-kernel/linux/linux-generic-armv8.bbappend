yhamamachi/meta-sparrow-hawk-xen

commit b7602167296c06a2864de40841f9cca8cb8ba227

dom0: Change to use same kernel as DomD/DomU
Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

meta-xen-dom0/recipes-kernel/linux/linux-generic-armv8.bbappend
@@ -1,0 +1,10 @@
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
+
+# From meta-sparrow-hawk
+require recipes-kernel/linux/kernel_6.12.inc
+
+SRC_URI = "\
+    ${REPO};branch=${BRANCH};protocol=https \
+    file://defconfig \
+"
+

prod-devel-rcar4_new.yaml
@@ -327,6 +327,10 @@ parameters:
           MACHINE: "sparrow-hawk"
           SOC_FAMILY: "r8a779g3"
         components:
+          dom0:
+            builder:
+              layers:
+                - "../meta-sparrow-hawk"
           domd:
             sources:
               - type: git

commit 26be3a8c929fe33c5d2a50ad6e780aa3f7993af3

domd: Fix kernel version sanity check error
kernel_6.12.inc has weak definision so that LINUX_VERSION is overwritten
by other recipes.
This patch fixes build error by defining LINUX_VERSION variable directly.

Fixes: b7602167296c06a2864de40841f9cca8cb8ba227

Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

Fixes #1

meta-xen-dom0/recipes-kernel/linux/linux-generic-armv8.bbappend
@@ -2,6 +2,8 @@ FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
 # From meta-sparrow-hawk
 require recipes-kernel/linux/kernel_6.12.inc
+# Workaround: Fix Kernel version sanity check error
+LINUX_VERSION = "6.12.58"
 
 SRC_URI = "\
     ${REPO};branch=${BRANCH};protocol=https \